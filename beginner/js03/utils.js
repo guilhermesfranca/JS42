23c793d4fb9982482325026a5b369a282bff9094
guilhermesfranca <guilhermesfranca@outlook.com>
2026-04-09 16:47:23 +0100

    starting utils.js contruction

---
+++ beginner/js03/utils.js
@@ -0,0 +1,24 @@
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+}
+
+
+function truncateText(str, lim) {
+    let trunc = "";
+    let i = 0;
+    while(i < lim)
+    {
+        trunc += str[i]
+        i++;
+    }
+    return trunc
+}
+
+function genRandomNum(range) {
+    return Math.floor(Math.random() * range) + 1;
+}
+
+/**TESTES */
+console.log(capitalize("teste"))
+console.log(truncateText("test", 3))
+console.log(genRandomNum(224));
